23bd6910035d41231cb1ed1b649ab09fa8db0c88
Sandeep <61156183+Sandeep19531@users.noreply.github.com>
2021-08-30 14:54:52 +0530

    Added changes

--- src/Data/ApiData.js
+++ src/Data/ApiData.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const Data = {};
 
 const fetchData = async () => {
-    const {data} = await axios.get('http://127.0.0.1:5000/api/products');
+    const {data} = await axios.get('https://kissan-mandi.herokuapp.com/api/products');
     Data = data;
     console.log(data.Cereals);
 }
